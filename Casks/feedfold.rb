9c30e36d9781ae7227da4c0fcd2793e1adbe8e50
egornomic <git@egornomic.xyz>
2026-08-31 21:30:39 +0200

    chore: update feedfold to 0.4.4

--- Casks/feedfold.rb
+++ Casks/feedfold.rb
@@ -1,13 +1,13 @@
 cask "feedfold" do
-  version "0.4.3"
-  sha256 "d790e6606de3ae542ba602ed4258779c5497d6e8fbc91654ab33eab95b5414e8"
+  version "0.4.4"
+  sha256 "ad3cf05e29cda0eb664ea26be08f596f34bc5b272e05a4e0834fa4ebbad1fcc9"
 
   url "https://github.com/egornomic/feedfold/releases/download/v#{version}/feedfold-#{version}-arm64.dmg"
   name "feedfold"
   desc "Quiet, keyboard-first feed reader"
   homepage "https://github.com/egornomic/feedfold"
 
-  depends_on :macos
+  depends_on macos: :monterey
   depends_on arch: :arm64
 
   app "feedfold.app"
