49b0dbcab2f3421a351f96c54193d1c4f3ec7401
egornomic <git@egornomic.xyz>
2026-09-14 16:08:43 +0200

    chore: update feedfold to 0.7.0

--- Casks/feedfold.rb
+++ Casks/feedfold.rb
@@ -1,6 +1,6 @@
 cask "feedfold" do
-  version "0.6.0"
-  sha256 "e3f1cbdfa091d72e1343f0a853b4590cf06a051b047a4cbb9a7c12b198b97c9b"
+  version "0.7.0"
+  sha256 "fff247ddf1389f5da0e2a81ca1f3cdaeab7dcba9e3de2a62069e5d1a2c301cf6"
 
   url "https://github.com/egornomic/feedfold/releases/download/v#{version}/feedfold-#{version}-arm64.dmg"
   name "feedfold"
